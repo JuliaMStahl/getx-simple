05ee2630bdd40301ba2c34ae384a05b131f4bae5
Julia Stahl <juliastahl@alunos.utfpr.edu.br>
2022-09-15 09:42:13 -0300

    Alteração na importação do pacote de tradução

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get_cli/core/locales.g.dart';
 import 'package:routes_lib2/app_pages.dart';
+import 'package:translation_lib2/locales.dart';
 
 void main() {
   runApp(GetMaterialApp(
